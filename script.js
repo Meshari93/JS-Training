2e234502c4a25ba422399d82cdab01d4a88d19b0
unknown <almeshari93@gmail.com>
2018-12-14 08:50:23 +0300

    Console log & statement & Comment

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
-document.write("this is external JS");
-alert("Thes is alert from external JS")
+ console.log("Hello Friend");
+//  ths is console log statement
+
+/*
+ document.write("Hello Document");
+ console.log("Hello Friend");
+ document.write("Hello Document");
+ */

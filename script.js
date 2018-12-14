0348e0735e09605d0ecbefeb4a304e5bbc08f9a1
unknown <almeshari93@gmail.com>
2018-12-14 10:30:20 +0300

    Numbers

--- script.js
+++ script.js
@@ -1,19 +1,40 @@
- // String Concatenation
+//Numbers
 
 
-//  first example
- let name = "meshari";
- let lastName = "Alghamdi";
- let fullName;
+let number = 4;
+let number2 = 2.25;
 
- console.log("Hello there your name is : " + name + lastName);
+let sum = number + number2;
+let sub = number - number2;
 
- fullName = name + " " + lastName;
+let mul = number * number2;
+let div = number / number2;
 
- console.log("Hello there your full name is : " + fullName);
 
- // second example
- let info = "google";
- let url = "http://www." + info + ".com"
+console.log(sum);
+console.log(sub);
+console.log(sub);
+console.log(div);
 
- console.log(url);
+let mod1 = 9;
+let mod2 = 5;
+
+let mod = mod1 % mod2;
+
+console.log(mod);
+
+let addition = 4+6+10*10;
+let addition2 = (4+6+10)*10;
+
+console.log(addition);
+console.log(addition2);
+
+let add = 10;
+
+add +=1;
+add ++;
+
+add -=1;
+add --;
+
+console.log(add);

b0ee7ef2bf904e778c11c16ff6b2297a01bef894
unknown <almeshari93@gmail.com>
2018-12-14 15:54:45 +0300

    If Else Statements

--- script.js
+++ script.js
@@ -1,21 +1,44 @@
- // Objects In Javascript 
+// If Else Statements
+// Conditional Statements In Javascript
+//Comparison operators
 
- let person = {
-     name: "Meshari",
-     lastName: 'Jabbar',
-     age: 25,
-     education: false,
-     wife: true,
-     sibling:['sister', 'brother'],
-     fullName: function () {
-         console.log("And the full name of the person is : " + person.name + " " + person.lastName )
-     }
- }
+//>,<, >=, <=, ==, ===, !=, !==
 
- let personName = person.name;
- let personLastName = person['lastName'];
+if(2>1){
+    console.log("I am math genious");
+}
 
- console.log(personName);
- console.log(personLastName);
+let num1 = 8;
+let num2 = 5;
 
- person.fullName();
+if (num1>=num2) {
+    console.log("First number was bigger than second or equal")
+}
+else {
+    console.log("First number was smaller than second")
+}
+
+
+let num3 = 6;
+let num4 = 8;
+if (num3!=num4) {
+    console.log("number are not equal")
+}
+else {
+    console.log("number are  equal")
+}
+
+let person1 = "John";
+let person2 = "bob";
+
+let person = "will";
+
+if (person === person1) {
+    console.log("this dude is : " + person1);
+} 
+else if(person === person2){
+    console.log("this dude is : " + person2);
+}
+else {
+    console.log("I dont who is this dude");
+}

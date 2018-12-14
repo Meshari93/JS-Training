e532f50dd41b78f116cbcfefa75aa9f66ccfe9aa
unknown <almeshari93@gmail.com>
2018-12-14 11:09:08 +0300

    Data Type in Java Script

--- script.js
+++ script.js
@@ -1,21 +1,16 @@
-// Primitive Data Types in Javascript
+ // Data Type in Java Script
 
-// String 
-let text = "this is text";
-//Number 
-let value = 123;
-//Boolean 
-let value1 = true;
-let value2 = false;
-// Null
-let number23 = null;
-// undefined
-let name;
-// Symbols(ES6)
+ //arrays, function, objects
 
-console.log(typeof text + " => " + text);
-console.log(typeof value + " => " + value);
-console.log(typeof value1 + " => " + value1);
-console.log(typeof value2 + " => " + value2);
-console.log(typeof number23 + " => " + number23);
-console.log(typeof name + " => " + name);
+ let fruits = ['apple', 'orange', 'banana'];
+ let code = function code (){
+    console.log("Hello World");
+ }
+let person = {
+    name : "Meshari",
+    lastName : "Alghamdi"
+}
+
+console.log(typeof fruits + " => " + fruits);
+console.log(typeof code + " => " + code);
+console.log(typeof person + " => " + person.name, person.lastName);

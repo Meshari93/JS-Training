6f9a62934bd30b8e91594b12647900c18de9093b
unknown <almeshari93@gmail.com>
2018-12-16 22:01:25 +0300

    for loop with array

--- script.js
+++ script.js
@@ -1,65 +1,17 @@
- // difference between array and object
-// array === list of value 
-// object === collection of values 
+// for loop with array
 
-// let zero = [];
-let names = ['john', 'bob', 'barry', 'olga', 'ben'];
-
-// let age = new Array (19, 40, 60);
-//  console.log(names);
-// names[0] = 'petter';
-
-// console.log(names[0]);
-// console.log(names);
-
-// let person = {
-//   name: 'bob'
+// for (let i = 0; i<10; i++){
+//   console.log(`Your number is ${i}`);
+  
 // }
 
-// console.log(person.name);
+let friends = ['bob', 'susy', 'john', 'eddy', 'loga'];
+for (let i = 0; i<friends.length; i++){
+  console.log(`Your friend  number  ${i+1} is ${friends[i]}`);  
+}
 
-// person.name = 'susy'
-// console.log(person.name);
+console.log("----------------------------------");
 
-// length 
-let result = names.length;  //Gets or sets the length of the array. This is a number one higher than the highest element defined in an array.
-
-console.log(result);
-
-console.log(names[names.length-1]);
-
-// reverse
-console.log(names.reverse());  //Reverses the elements in an Array.
-names.reverse();
-
-//concat
-let lastNames = ['pepper', 'onion', 'banaba'];
-console.log(names.concat(lastNames));  //Combines two or more arrays.
-
-//nushift
-console.log(names.unshift('susy')); //Inserts new elements at the start of an array.
-console.log(names);
-
-//shift
-console.log(names.shift()); //Removes the first element from an array and returns it.
-console.log(names);
-
-//push
-console.log(names.push('hello world'));  //Appends new elements to an array, and returns the new length of the array.
-console.log(names);
-
-//pop
-console.log(names.pop()); //Removes the last element from an array and returns it.
-console.log(names);
-
-//splice
-//  console.log(names.splice(2,3)); //Removes elements from an array and, if necessary, inserts new elements in their place, returning the deleted elements.
-//  console.log(names);
-
-//sort 
-console.log(names.sort()); //Sorts an array.
-console.log(names);
-
-
-
-
+for (let j = friends.length-1; j>=0; j--){
+  console.log(`Your friend  number  ${j+1} is ${friends[j]}`);  
+}

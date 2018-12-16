fd4e8402e9226f83b5a8e4dbd0bdc5effd286b58
unknown <almeshari93@gmail.com>
2018-12-16 17:21:31 +0300

    for loop

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
   // repeatedly run a block of cade while certain condition is true 
-  // do loop
+  // for  loop
 
-  let money = 0;
+  let i;
 
-  do {
-     console.log("You have " + money + " and you are going to the mall");
-     money++;
-  } while(money<10);
   

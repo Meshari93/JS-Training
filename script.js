d91d0d15d0b813152b8d1baa95b804e009899d13
unknown <almeshari93@gmail.com>
2018-12-16 20:37:44 +0300

    String

--- script.js
+++ script.js
@@ -1,13 +1,41 @@
-// both represent "no-value"
+// String 
+let name = 'meshari';
+let lastName = 'jabbar';
 
-// undefined ---- " javascript can not find value for this"
+let fullName = name + ' ' + lastName; // Concatination
 
-//null -----"programmer set the value"
+console.log(fullName);
 
-let number = 20 +null; //20 + 0
+let person = {
+  name : 'meshari',
+  lastName : 'jabbar',
+  fullName : function () {  //methods
+    console.log(this.name + " " + this.lastName);    
+  }
+}
 
-console.log(number);
+console.log(person.name);
 
-let number2 = 20 + undefined;
+let human = "Meshari Jabbar";
+let length = human.length;
 
-console.log(number2);
+console.log(human.length);
+
+console.log(human.toLowerCase());
+console.log(human.toUpperCase());
+
+console.log(human.charAt(0));
+console.log(human.charAt(human.length-1));
+
+console.log(human.indexOf('e'));
+
+console.log(human.toLocaleLowerCase().startsWith('meshari'));
+console.log(human.toLocaleLowerCase().endsWith('jabbar'));
+
+console.log(human.slice(0,3));
+console.log(human.slice(-5));
+
+console.log(human.includes('zzz'));
+
+
+

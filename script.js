47925381627bd4841297357c5c5caf4967f912dd
unknown <almeshari93@gmail.com>
2018-12-16 16:19:40 +0300

    Switch traning

--- script.js
+++ script.js
@@ -1,23 +1,41 @@
-//  Switch && if
- let dice =1;
+//  Switch  
 
-//  if (dice===1) {
-//      console.log("You got one");
-//  }
-//  else if (dice===2) {
-//      console.log("You got two");     
-//  }
-//  else{
-//      console.log("You did not roll the dice");
-//  }
+function displayDayOfTheWeek(day) {
+    switch(day){
+        case 1:
+        console.log('Today is Monday');
+        break;
+        case 2:
+        console.log('Today is Tuesday');
+        break;
+        case 3:
+        console.log('Today is Wednsday');
+        break;
+        case 4:
+        console.log('Today is Thursday');
+        break;
+        case 5:
+        console.log('Today is Friday');
+        break;
+        case 6:
+        console.log('Today is Saturday');
+        break;
+        case 7:
+        console.log('Today is Sunday');
+        break;
+        default:
+        console.log('INVALID DAY !!!!!!');
+    }
+}
 
-switch(dice){
-    case 1:
-    console.log("You got one");
-    break;
-    case 2:
-    console.log("You got two");
-    break;
-    default:
-    console.log("You did not roll the dice");
- }
+displayDayOfTheWeek(1);
+displayDayOfTheWeek(2);
+displayDayOfTheWeek(3);
+displayDayOfTheWeek(4);
+displayDayOfTheWeek(5);
+displayDayOfTheWeek(6);
+displayDayOfTheWeek(7);
+displayDayOfTheWeek(0);
+
+displayDayOfTheWeek(0);
+

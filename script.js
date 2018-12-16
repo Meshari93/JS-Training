dd879eef190d9a4e4fbc6974af33713b5a8a440f
unknown <almeshari93@gmail.com>
2018-12-16 17:27:58 +0300

    for loop

--- script.js
+++ script.js
@@ -3,4 +3,11 @@
 
   let i;
 
-  
+  for(i=0;i<10;i++) {
+      console.log("And the number is " + i);
+      
+  }
+
+for (let number = 11; number >= 0; number--) {
+    console.log("And the nnumber is " + number);
+}
